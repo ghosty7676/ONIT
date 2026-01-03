54f05c47806608e74c10a5e91b1485e0ed89e89f
Kelechi Ebiri <ebiritg@gmail.com>
2026-01-03 20:42:59 +0100

    refactor health endpoint

--- server/controllers/healthCheck.js
+++ server/controllers/healthCheck.js
@@ -1,5 +1,13 @@
+import { dbClient } from "../server";
+
 async function healthCheck(req, res) {
-  res.send("Every good");
+  try {
+    await dbClient.query("SELECT 1");
+    res.send("Everywhere good");
+    res.status(200).json({ status: "ok" });
+  } catch {
+    res.status(500).json({ status: "db down" });
+  }
 }
 
 export { healthCheck };
