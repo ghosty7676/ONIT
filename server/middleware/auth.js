3492cc195997abdd783ff8b42357db5b3eeed21a
Kelechi Ebiri <ebiritg@gmail.com>
2026-01-05 23:06:16 +0100

    feat: Implement auth

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+import redisClient from "../utils/redis";
+import dbClient from "../server";
+
+export async function auth(req, res, next) {
+  const token = req.headers["x-token"];
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+  const userId = await redisClient.get(`auth_${token}`);
+  if (!userId) return res.status(401).json({ error: "Unauthorized" });
+
+  const result = await dbClient.query(
+    "SELECT id,  role, is_blocked FROM users WHERE id = $1",
+    [userId]
+  );
+
+  const user = result.row[0];
+  if (!user || user.is_blocked) {
+    return res.status(403).json({ error: "Access denied" });
+  }
+
+  req.user = user;
+  next();
+}
