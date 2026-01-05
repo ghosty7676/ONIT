f80b90afa973abe834840b6cf76eb2ae03d77076
Kelechi Ebiri <ebiritg@gmail.com>
2026-01-05 23:56:00 +0100

    refactor routes index

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,22 +4,9 @@
  */
 
 import express from "express";
-import { getConnect, getDisconnect } from "../controllers/AuthController.js";
+import authRoutes from "./auth.js";
 
 const router = express.Router();
 
-/**
- * Route for connecting to a user.
- * @name GET /connect
- * @function
- */
-router.get("/connect", getConnect);
-
-/**
- * Route for disconnecting from a user.
- * @name GET /disconnect
- * @function
- */
-router.get("/disconnect", getDisconnect);
-
+router.use("/auth", authRoutes);
 export default router;
